cf052914bcece5ba06ee02ae39dcc22b287eccfb
vijay kumar <vijay84049@gmail.com>
2024-10-24 08:48:13 +0530

    Model For Book And Transaction/ Issued Book

---
+++ Models/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace LMS.Models
+{
+    
+        public class Book
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public int TotalCopies { get; set; }
+            public int AvailableCopies { get; set; }
+        }
+
+        public class IssuedBook
+        {
+            public int IssuedBookId { get; set; }
+            public int BookId { get; set; }
+            public string IssuedTo { get; set; }
+            public DateTime IssueDate { get; set; }
+            public DateTime? ReturnDate { get; set; }
+            public Book Book { get; set; }
+        }
+
+    }
+
